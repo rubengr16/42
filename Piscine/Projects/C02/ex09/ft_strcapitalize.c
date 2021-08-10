64eb514f75ba27fed5a8a7fb947a780b291f4992
rubengr16 <ruben16gallego@gmail.com>
2021-08-10 15:37:39 +0200

    capitalice updated

--- Piscine/Projects/C02/ex09/ft_strcapitalize.c
+++ Piscine/Projects/C02/ex09/ft_strcapitalize.c
@@ -6,13 +6,18 @@
 /*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 14:38:06 by rgallego          #+#    #+#             */
-/*   Updated: 2021/08/10 15:12:28 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/08/10 15:36:55 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	to_up(char c)
 {
 	return (c - 'a' + 'A');
+}
+
+char	to_low(char c)
+{
+	return (c -'A' + 'a');
 }
 
 char	*ft_strcapitalize(char *str)
@@ -28,8 +33,13 @@ char	*ft_strcapitalize(char *str)
 		if (!(('0' <= str[cont - 1] && str[cont - 1] <= '9')
 				|| ('a' <= str[cont - 1] && str[cont - 1] <= 'z')
 				|| ('A' <= str[cont - 1] && str[cont - 1] <= 'Z')))
+		{
 			if ('a' <= str[cont] && str[cont] <= 'z')
 				str[cont] = to_up(str[cont]);
+		}
+		else
+			if ('A' <= str[cont] && str[cont] <= 'Z')
+				str[cont] = to_low(str[cont]);
 		cont++;
 	}
 	return (str);
--- Piscine/Projects/C02/ex09/main.c
+++ Piscine/Projects/C02/ex09/main.c
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 15:00:31 by rgallego          #+#    #+#             */
-/*   Updated: 2021/08/10 15:13:58 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/08/10 15:36:37 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strcapitalize(char *str);
 
 int	main(void)
 {
-	char	str[] = "he, hi, .k, 8hdjh";
+	char	str[] = "salut, comment tu vas ? 42mots quaRante-deux; cinquante+et+un";
 
 	printf("%s\n", str);
 	printf("%s", ft_strcapitalize(str));
