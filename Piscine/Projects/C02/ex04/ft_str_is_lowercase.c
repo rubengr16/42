dbf175a858294db4fe04e3fd845da9a9da01c34a
rubengr16 <ruben16gallego@gmail.com>
2021-08-10 12:24:33 +0200

    ex04

--- Piscine/Projects/C02/ex04/ft_str_is_lowercase.c
+++ Piscine/Projects/C02/ex04/ft_str_is_lowercase.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 11:41:10 by rgallego          #+#    #+#             */
-/*   Updated: 2021/08/10 12:06:04 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/08/10 12:22:10 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_alpha(char *str)
+int	ft_str_is_lowercase(char *str)
 {
 	int	alpha;
 
 	alpha = 1;
 	while (str[alpha - 1] != '\0' && alpha)
 	{
-		if (('a' <= str[alpha - 1] && str[alpha - 1] <= 'z')
+		if ('a' <= str[alpha - 1] && str[alpha - 1] <= 'z')
 			alpha++;
 		else
 			alpha = 0;
