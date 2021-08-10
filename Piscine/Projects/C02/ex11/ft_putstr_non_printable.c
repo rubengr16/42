901c71053484b06e64b7be3d47a1ed5aba63561b
rubengr16 <ruben16gallego@gmail.com>
2021-08-10 19:51:40 +0200

    ft_str_non_printable

--- Piscine/Projects/C02/ex11/ft_putstr_non_printable.c
+++ Piscine/Projects/C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 18:00:05 by rgallego          #+#    #+#             */
-/*   Updated: 2021/08/10 19:03:19 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/08/10 19:49:57 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,65 +22,31 @@ int	ft_is_not_printable(char c)
 	return (resul);
 }
 
-char	dec_to_hex(int num)
+void	ft_putchar(char c)
 {
-	if (num < 10)
-		num = num + '0';
-	else
-		num = (num % 10) + 'a';
-	return (num);
+	write(1, &c, 1);
 }
 
-void	to_hex(int num, char *hex)
+void	to_hex(int num)
 {
-	char	aux[3];
-	int		cont;
-	int		aux_cont;
+	char	*hex;
 
-	cont = 0;
-	while (num > 16)
-	{
-		aux[cont] = dec_to_hex(num % 16);
-		num /= 16;
-		cont++;
-	}
-	aux[cont] = dec_to_hex(num);
-	hex[0] = '\\';
-	aux_cont = 1;
-	while (aux_cont <= cont)
-	{
-		hex[4 - cont] = aux[cont];
-		aux_cont++;
-	}
-	hex[aux_cont + 1] = '\0';
-}
-
-void	print_hex(char *str)
-{
-	char	*aux;
-
-	aux = str;
-	while (*aux != '\0')
-	{
-		write(1, aux, 1);
-		aux++;
-	}
+	hex = "0123456789abcdef";
+	ft_putchar('\\');
+	ft_putchar(hex[num / 16]);
+	ft_putchar(hex[num % 16]);
 }
 
 void	ft_putstr_non_printable(char *str)
-{
-	int		chrnbr;
+{	
 	int		cont;
-	char	hex[4];
 
 	cont = 0;
 	while (str[cont] != '\0')
 	{
 		if (ft_is_not_printable(str[cont]))
 		{
-			chrnbr = str[cont];
-			to_hex(chrnbr, hex);
-			print_hex(hex);
+			to_hex(str[cont]);
 		}
 		else
 			write(1, &str[cont], 1);
--- Piscine/Projects/C02/ex11/main.c
+++ Piscine/Projects/C02/ex11/main.c
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 18:36:37 by rgallego          #+#    #+#             */
-/*   Updated: 2021/08/10 19:04:00 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/08/10 19:50:27 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_putstr_non_printable(char *str);
 
 int	main(void)
 {
-	char	str[] = "hello\n, there\t, dude\t, bye";
+	char	str[] = "hello\n, there\t, dude\b, bye";
 
 
 	printf("%s\n", str);
