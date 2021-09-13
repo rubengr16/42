d7abaf0bc6bd3dd28b4bf1e50a59e5256053139b
rgallego <rgallego@student.42madrid.com>
2021-09-13 18:13:15 +0200

    memmove fails

---
+++ 42cursus/Libft/ft_memmove.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/13 16:17:52 by rgallego          #+#    #+#             */
+/*   Updated: 2021/09/13 17:53:44 by rgallego         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t		cnt;
+	char		*str_dst;
+	const char	*str_src;
+
+	cnt = 0;
+	str_dst = dst;
+	str_src = src;
+	if (dst != src)
+	{
+		if (src < dst)
+			while (*str_dst != str_src[cnt])
+				cnt++;
+		if (cnt < len)
+			ft_memcpy(dst, src, len);
+		else
+		{
+			ft_memcpy(&dst[cnt], dst, len - cnt);
+			ft_memcpy(dst, src, cnt);
+		}
+	}
+	return (dst);
+}
