cba4a188fb28040e875c8f2ff59f85aa8896061a
rgallego <rgallego@student.42madrid.com>
2021-10-22 20:42:04 +0200

    ft_printf_ptr improved

--- 42cursus/ft_printf/ft_printf_ptr.c
+++ 42cursus/ft_printf/ft_printf_ptr.c
@@ -6,13 +6,16 @@
 /*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 20:11:17 by rgallego          #+#    #+#             */
-/*   Updated: 2021/10/22 20:23:42 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/10/22 20:41:29 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_printf_ptr(long long ptr)
+int	ft_printf_ptr(void *ptr)
 {
-	return (ft_putnbr_base(ptr, "0123456789abcdef", 16));
+	if (ptr)
+		return ((long long)ft_putnbr_base(ptr, "0123456789abcdef", 16));
+	else
+		return (write(1, "(null)", 6);
 }
