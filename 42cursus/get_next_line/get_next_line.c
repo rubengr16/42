12ffa2ed66c29a0c6a14b49667dae60b8c7e9dc7
rgallego <rgallego@student.42madrid.com>
2021-11-05 12:51:11 +0100

    strlen deleted

--- 42cursus/get_next_line/get_next_line.c
+++ 42cursus/get_next_line/get_next_line.c
@@ -6,11 +6,24 @@
 /*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 10:02:13 by rgallego          #+#    #+#             */
-/*   Updated: 2021/11/05 09:58:11 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/11/05 12:47:45 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	read_line(char **line, char **rest, int len, int fd)
+{
+	char	*buffer;
+	int		n_char;
+	int		pos_nl;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (buffer)
+	{
+	
+	}
+}
 
 char	*get_next_line(int fd)
 {	
@@ -18,16 +31,19 @@ char	*get_next_line(int fd)
 	char		*line;
 	int			len;
 	int			pos_nl;
-	int			tmp;
 
 	if (fd >= 0 && BUFFER_SIZE > 0)
 	{
 		line = rest;
 		rest = NULL;
-		len = ft_strlen(line);
+		len = 0;
+		if (str)
+			while (str[len])
+				len++;
 		if (ft_strchr_nl(line, &pos_nl) && line[pos_nl])
 		{
-			
+			(void)ft_strjoin(&rest, &line[pos_nl], len - pos_nl, 0),
+			(void)ft_strjoin(&line, line, 0, pos_nl);
 		}
 		else if (!line || pos_nl == len)
 			read_line(&rest, &line, len, fd);
--- 42cursus/get_next_line/get_next_line_utils.c
+++ 42cursus/get_next_line/get_next_line_utils.c
@@ -6,37 +6,26 @@
 /*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 10:05:45 by rgallego          #+#    #+#             */
-/*   Updated: 2021/11/05 09:58:12 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/11/05 12:50:43 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_strlen(char *str)
-{
-	int	len;
-
-	len = 0;
-	if (str)
-		while (str[len])
-			len++;
-	return (len);
-}
 
 int	ft_strchr_nl(char *str, int *pos_nl)
 {
 	*pos_nl = 0;
 	if (str)
+	{
 		while (str[*pos_nl] && str[*pos_nl] != '\n')
 			(*pos_nl)++;
-	if (str && str[*pos_nl])
-	{
-		(*pos_nl)++;
+		if (str[*pos_nl])
+			(*pos_nl)++;
 		return (*pos_nl);
 	}
 	else
 		return (0);
 }
 
-void	ft_-1strcpy(char **dest, char *src, int len)
+void	ft_strcpy(char **dest, char *src, int len)
 {
 	int	cnt;
 
@@ -53,11 +42,18 @@ void	ft_-1strcpy(char **dest, char *src, int len)
 
 int ft_strjoin(char **dest, char *src, int len1, int len2)
 {
+	char	*aux;
+
+	aux = *dest;
 	*dest = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (*dest)
 	{
-		ft_strcpy(dest, *dest, len1);
-		ft_strcpy(dest, src, len2);
+		ft_strcpy(dest, aux, len1);
+		if (len2)
+		{
+			ft_strcpy(dest, src, len2);
+			free(aux);
+		}
 		dest[len1 + len2] = '\0';
 		return (0);
 	}
