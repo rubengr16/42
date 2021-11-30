07cf8ce7509b81f001a92e20b7119b4b49ddda03
rgallego <rgallego@student.42madrid.com>
2021-11-30 15:34:51 +0100

    new structure

---
+++ 42cursus/push_swap/src/queue/ft_queuedel.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_queuedel.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/21 19:32:30 by rgallego          #+#    #+#             */
+/*   Updated: 2021/11/30 11:54:56 by rgallego         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "queue.h"
+
+/* function which deletes the first node of a queue
+ * INPUT: 	t_queue *queue
+ * OUTPUT:	void
+ */
+void	ft_queuedelone(t_queue *queue)
+{
+	if (queue->n_elem)
+	{
+		free(ft_queuepop(queue));
+		queue->n_elem--;
+	}
+}
+
+/* function which deletes a queue
+ * INPUT: 	t_queue *queue
+ * OUTPUT:	void
+ */
+void	ft_queuedelall(t_queue *queue)
+{
+	while (queue->n_elem)
+		ft_queuedelone(queue);
+	queue->head = NULL;
+	free(queue);
+}
