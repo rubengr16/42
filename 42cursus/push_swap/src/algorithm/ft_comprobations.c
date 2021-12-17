ee6388f7e294fe663103da09a800864933d572f6
rgallego <rgallego@student.42madrid.com>
2021-12-17 20:37:47 +0100

    base case ok, normi left

--- 42cursus/push_swap/src/algorithm/ft_comprobations.c
+++ 42cursus/push_swap/src/algorithm/ft_comprobations.c
@@ -6,16 +6,18 @@
 /*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 19:00:58 by rgallego          #+#    #+#             */
-/*   Updated: 2021/12/17 19:07:28 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/12/17 20:12:01 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "algorithm.h"
 
 int	ft_issorted(t_queue *queue)
 {
 	t_node	*aux;
 
 	aux = queue->head->next;
-	while (aux != queue->head && aux->next > aux)
+	while (aux != queue->head && aux->num > aux->prvs->num)
 		aux = aux->next;
 	if (aux == queue->head)
 		return (1);
--- 42cursus/push_swap/src/algorithm/ft_mysort.c
+++ 42cursus/push_swap/src/algorithm/ft_mysort.c
@@ -6,17 +6,21 @@
 /*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 20:00:20 by rgallego          #+#    #+#             */
-/*   Updated: 2021/12/17 19:24:59 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/12/17 20:12:05 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "algorithm.h"
 
 void	ft_sortbase_a(t_queue *a)
 {
 	if (!ft_issorted(a))
 	{
-		if (a->head < a->head->next && a->head->next > a->head->prvs)
+		if (a->head->num < a->head->next->num
+			&& a->head->next->num > a->head->prvs->num)
 			ft_reverse_rotate_a(a);
-		else if (a->head->next < a->head && a->head > a->head->prvs)
+		else if (a->head->next->num < a->head->num
+			&& a->head->num > a->head->prvs->num)
 			ft_rotate_a(a);
 		if (!ft_issorted(a))
 			ft_swap_a(a);	
