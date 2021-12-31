8e7545670ab9ea15b8f6cc41010a82e6e0cb33b2
rubengr16 <ruben16gallego@gmail.com>
2021-12-31 18:47:33 +0100

    error management ok, but still WIP

--- 42cursus/pipex/inc/pipex.h
+++ 42cursus/pipex/inc/pipex.h
@@ -6,15 +6,18 @@
 /*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 19:24:05 by rgallego          #+#    #+#             */
-/*   Updated: 2021/12/31 12:06:31 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/12/31 18:44:44 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# define CMD 0
 # define FLAG 1
-# define BAD_NB_ARGC 1
+
+# define ERR_CMD 1
+# define ERR_NB_ARGC 2
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -23,9 +26,9 @@
 /********************************* UTILS *********************************** */
 void	free_set(char **set);
 char	*ft_strjoinsep(char const *s1, char const *s2, char *c);
-void	error_msg(int	error);
+void	error_msg(int	error, char *str);
 
 /********************************* PIPEX *********************************** */
-char	*isvalidcmd(char *cmd, char **envp);
+char	*isvalidcmd(char **cmd, char **envp);
 
 #endif
--- 42cursus/pipex/src/pipex.c
+++ 42cursus/pipex/src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 19:30:34 by rgallego          #+#    #+#             */
-/*   Updated: 2021/12/31 12:05:21 by rgallego         ###   ########.fr       */
+/*   Updated: 2021/12/31 18:45:54 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,39 +21,59 @@ void	separate_flag(char ****set_of_cmds, char **argv)
 	(*set_of_cmds)[2] = NULL;
 }
 
-void	error_msg(int error)
+void	error_msg(int error, char *str)
 {
-	if (error == BAD_NB_ARGC)
-		ft_putendl_fd("Enter 4 arguments with the following structure:", 1);
-	ft_putendl_fd("./pipex file1 cmd1 cmd2 file2", 1);
+	if (error == ERR_CMD)
+	{
+		ft_putstr_fd(str, 1);
+		ft_putendl_fd(": command not found", 1);
+	}
+	else if (error == ERR_NB_ARGC)
+	{	
+		ft_putendl_fd("Please, enter 4 arguments with the structure:", 1);
+		ft_putendl_fd("./pipex file1 cmd1 cmd2 file2", 1);
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	***set_of_cmds;
-	char	*path;
 	int		i = 0;
 	int		j = 0;
 
 	(void)envp;
-	if (argc != 5)
-		error_msg(BAD_NB_ARGC);
-	set_of_cmds = NULL;
-	separate_flag(&set_of_cmds, argv);
-	while (set_of_cmds[i])
+	if (argc == 5)
 	{
-		j = 0;
-		while (set_of_cmds[i][j])
+		set_of_cmds = NULL;
+		separate_flag(&set_of_cmds, argv);
+		if (isvalidcmd(&(set_of_cmds[0][CMD]), envp))
 		{
-			printf("\n%s", set_of_cmds[i][j]);
-			j++;
+			if (isvalidcmd(&(set_of_cmds[1][CMD]), envp))
+			{
+				while (set_of_cmds[i])
+				{
+					j = 0;
+					while (set_of_cmds[i][j])
+					{
+						printf("\n%s", set_of_cmds[i][j]);
+						j++;
+					}
+					i++;
+				}
+				//perror("Fuck u");
+				execve(set_of_cmds[0][CMD], set_of_cmds[0], envp);
+			} 
+			else
+				error_msg(ERR_CMD, set_of_cmds[1][CMD]);
 		}
-		i++;
+		else
+			error_msg(ERR_CMD, set_of_cmds[0][CMD]);
 	}
-	//perror("Fuck u");
-	path = isvalidcmd(set_of_cmds[0][0], envp);
-	execve(path, set_of_cmds[0], envp);
-/*	int i = 0;
+	else
+		error_msg(ERR_NB_ARGC, "");
+
+/*	
+ *	int i = 0;
 	char *str;
 
 	(void)argc;
@@ -104,28 +124,30 @@ static void	try_path(char **set_of_paths, char **path, char *cmd, int *cnt)
  * OUTPUT:	int	:	1	command is valid
  * 					0	invalid command
  */
-char	*isvalidcmd(char *cmd, char **envp)
+char	*isvalidcmd(char **cmd, char **envp)
 {
 	int		cnt;
 	char	*path;
 	char	**set_of_paths;
 
 	cnt = 0;
-	while (envp[cnt]
-		&& !ft_strnstr(envp[cnt], "PATH=", 5))
+	path = NULL;
+	while (envp[cnt] && !ft_strnstr(envp[cnt], "PATH=", 5))
 		cnt++;
 	if (envp[cnt])
 	{
 		set_of_paths = ft_split(&envp[cnt][5], ':');
 		if (set_of_paths)
 		{
-			try_path(set_of_paths, &path, cmd, &cnt);
+			try_path(set_of_paths, &path, *cmd, &cnt);
 			free_set(set_of_paths);
 			if (!set_of_paths[cnt])
 			{
 				free(path);
 				path = NULL;
 			}
+			else
+				*cmd = path;
 		}
 	}
 	return (path);
