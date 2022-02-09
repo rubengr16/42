a7111e45ba79f8d03be152a8531bd9ed7b8619a7
rgallego <rgallego@student.42madrid.com>
2022-02-09 17:08:19 +0100

    .h modified

--- 42cursus/pipex/inc/pipex.h
+++ 42cursus/pipex/inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 19:24:05 by rgallego          #+#    #+#             */
-/*   Updated: 2022/01/02 15:38:42 by rgallego         ###   ########.fr       */
+/*   Updated: 2022/01/21 11:45:03 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,29 +28,29 @@
 # define PIPE_RD 0
 # define PIPE_WR 1
 
-# include <sys/wait.h> /*** pid_t	wait(int *wsatus); ***/
-# include <stdio.h> /*** void	perror(const char *s); ***/
+# include <sys/wait.h>	/***	pid_t	wait(int *wsatus);	***/
+# include <stdio.h>		/***	void	perror(const char *s);	***/
 //# include <errno.h> /*** error	constants ***/
-# include <unistd.h> /*** int	dup2(int oldfd, int newfd);
-						  pid_t	fork(void);
-						  int pipe(int pipefd[2]); ***/
+# include <unistd.h>	/***	int	dup2(int oldfd, int newfd);
+						  		pid_t	fork(void);
+						  		int pipe(int pipefd[2]);	***/
 # include <stdlib.h>
 # include "libft.h"
 
 /*****************************  *********************************** */
 typedef struct s_args
 {
-	int		fdin;
-	int		fdout;
+	int		fd_in;
+	int		fd_out;
 	int		n_cmds;
-	char	***set_of_cmds;
+	char	***cmds;
 }	t_args;
 
 /********************************* UTILS *********************************** */
 void	free_set(char **set);
 void	free_set_of_cmd(t_args args);
 void	error_msg(t_args args, char *str, int error);
-void	separate_flag(t_args *args, char **argv, int argc);
+void	separate_flag(t_args *args, char **argv);
 char	*ft_strjoinsep(char const *s1, char const *s2, char *c);
 
 /********************************* PIPEX *********************************** */
