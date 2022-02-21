4c4a70145052ccc8f536aa55cb4176ad4510efce
rgallego <rgallego@student.42madrid.com>
2022-02-21 15:06:00 +0100

    norminette ok

--- 42cursus/get_next_line/src/get_next_line_bonus.c
+++ 42cursus/get_next_line/src/get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 10:02:13 by rgallego          #+#    #+#             */
-/*   Updated: 2022/02/21 14:36:54 by rgallego         ###   ########.fr       */
+/*   Updated: 2022/02/21 15:05:30 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ char	*get_next_line(int fd)
 				len++;
 		if (ft_strchr_nl(line, &pos_nl) && (pos_nl + 1) < len)
 		{
-			(void)strjoin_gnl(&rest[fd], &line[pos_nl + 1], 0, len - pos_nl - 1);
+			strjoin_gnl(&rest[fd], &line[pos_nl + 1], 0, len - pos_nl - 1);
 			(void)strjoin_gnl(&line, line, 0, pos_nl);
 		}
 		else if (!line || pos_nl == len)
