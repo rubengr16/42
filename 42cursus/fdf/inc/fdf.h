1ef25965ae74c47b00a1d33e1989a932b7dd3894
rgallego <rgallego@student.42madrid.com>
2022-02-23 18:16:18 +0100

    structure decided

--- 42cursus/fdf/inc/fdf.h
+++ 42cursus/fdf/inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 20:30:51 by rgallego          #+#    #+#             */
-/*   Updated: 2022/02/21 15:30:20 by rgallego         ###   ########.fr       */
+/*   Updated: 2022/02/23 18:15:07 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,21 @@
 # include "libft.h"
 # include "get_next_line.h"
 
-/******************************** STRUCTURE ********************************* */
+/******************************** CONSTANTS ********************************* */
 # define FILE 1
 
 /******************************** STRUCTURE ********************************* */
+typedef struct s_cell
+{
+	int	z;
+	int	colour;
+} t_cell;
+
 typedef struct s_map
 {
-
+	int		size_x;
+	int		size_y;
+	t_cell	**matrix;
 }	t_map;
 
 /********************************** UTILS *********************************** */
