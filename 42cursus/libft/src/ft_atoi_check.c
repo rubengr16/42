e0d870e9f6238139d1ee728e02483705b354fadc
rgallego <rgallego@student.42madrid.com>
2022-03-07 12:50:41 +0100

    ft_atoi_check and ft_free_split added as extra functions

---
+++ 42cursus/libft/src/ft_atoi_check.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_check.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgallego <rgallego@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/07 12:24:28 by rgallego          #+#    #+#             */
+/*   Updated: 2022/03/07 12:48:55 by rgallego         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+ * function decides if the number represented by the string is inside the range 
+ * values of int
+ * INPUT:	int nb, char *str
+ * OUTPUT:	int	:	1	number cannot be represented 
+ * 					0	number is correct
+ */
+int	ft_atoi_check(int nb, char *str)
+{
+	if ((!nb || nb != -1) && (ft_strlen(str) > 2))
+		return (1);
+	else
+		return (0);
+}
