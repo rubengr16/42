2aa155daf2370f4a06d5958a04201776b8691b78
rgallego <rgallego@student.42madrid.com>
2022-03-22 12:22:41 +0100

    clean horizontal() and vertical() issue #1

--- 42cursus/fdf/src/draw_map.c
+++ 42cursus/fdf/src/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 18:12:51 by rgallego          #+#    #+#             */
-/*   Updated: 2022/03/18 20:32:30 by rgallego         ###   ########.fr       */
+/*   Updated: 2022/03/22 12:21:23 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,25 +34,6 @@ static void	bresenham(t_mlx_data mlx, t_point start_pt, t_point end_pt, int colo
 		my_pixel_put(&mlx.img, start_pt.x, start_pt.y, colour);
 	}
 }
-static void	horizontal(t_mlx_data mlx, t_point start_pt, t_point end_pt, int colour)
-{
-	my_pixel_put(&mlx.img, start_pt.x, start_pt.y, colour);
-	while (start_pt.x < end_pt.x)
-	{
-		start_pt.x++;
-		my_pixel_put(&mlx.img, start_pt.x, start_pt.y, colour);
-	}
-}
-
-static void	vertical(t_mlx_data mlx, t_point start_pt, t_point end_pt, int colour)
-{
-	my_pixel_put(&mlx.img, start_pt.x, start_pt.y, colour);
-	while (start_pt.y < end_pt.y)
-	{
-		start_pt.y++;
-		my_pixel_put(&mlx.img, start_pt.x, start_pt.y, colour);
-	}
-}
 
 void	draw(t_fdf *fdf)
 {
@@ -66,9 +47,9 @@ void	draw(t_fdf *fdf)
 		while (j < fdf->map.x)
 		{
 			if (j > 0)
-				horizontal(fdf->mlx, get_pt(j - 1, i, *fdf), get_pt(j, i, *fdf), fdf->map.matrix[i][j].colour);
+				bresenham(fdf->mlx, get_pt(j - 1, i, *fdf), get_pt(j, i, *fdf), fdf->map.matrix[i][j].colour);
 			if (i > 0)
-				vertical(fdf->mlx, get_pt(j, i - 1, *fdf), get_pt(j, i, *fdf), fdf->map.matrix[i][j].colour);
+				bresenham(fdf->mlx, get_pt(j, i - 1, *fdf), get_pt(j, i, *fdf), fdf->map.matrix[i][j].colour);
 			if (i > 0 && j > 0)
 				bresenham(fdf->mlx, get_pt(j - 1, i - 1, *fdf), get_pt(j, i, *fdf), fdf->map.matrix[i][j].colour);
 			j++;
