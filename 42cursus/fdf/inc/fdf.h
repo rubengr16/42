942882e57e15f31cf407c63fc33ff24a209e637b
rgallego <rgallego@student.42madrid.com>
2022-04-06 18:38:44 +0200

    change BASE to hexup - issue #9

--- 42cursus/fdf/inc/fdf.h
+++ 42cursus/fdf/inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 20:30:51 by rgallego          #+#    #+#             */
-/*   Updated: 2022/04/06 18:01:16 by rgallego         ###   ########.fr       */
+/*   Updated: 2022/04/06 18:36:25 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@
 # define WIN_Y 720
 # define X0 300
 # define Y0 300
-# define BASE "0123456789abcdef"
+# define BASE "0123456789ABCDEF"
 # define WHITE 0x00FFFFFF
 # define Z 0
 # define COLOUR	1
