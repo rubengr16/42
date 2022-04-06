9cd788d41e652c21dfa27e9a99c2e8ab19d3694a
rgallego <rgallego@student.42madrid.com>
2022-04-06 18:47:59 +0200

    check norminette on main.c - issue #15

--- 42cursus/fdf/src/main.c
+++ 42cursus/fdf/src/main.c
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 13:59:49 by rgallego          #+#    #+#             */
-/*   Updated: 2022/03/23 18:52:52 by rgallego         ###   ########.fr       */
+/*   Updated: 2022/04/06 18:47:01 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ int	main(int argc, char **argv)
 	t_fdf	fdf;
 	int		fdin;
 
-	fdf = (t_fdf){{NULL, NULL, {NULL, NULL, -1, -1, -1}}, {-1, 0, 5, X0, Y0, NULL}};
+	fdf = (t_fdf){{NULL, NULL, {NULL, NULL, -1, -1, -1}}, \
+		{-1, 0, 5, X0, Y0, NULL}};
 	atexit(leaks);
 	if (argc != 2)
 		error_msg("Please, enter 2 arguments", ERR_USR);
