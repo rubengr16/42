246e1d3c43510fafa4a2e0386c123e4560fc9f8b
rgallego <rgallego@student.42madrid.com>
2022-04-08 17:50:33 +0200

    create keys.h and add fdf.h - #25

--- 42cursus/fdf/inc/fdf.h
+++ 42cursus/fdf/inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 20:30:51 by rgallego          #+#    #+#             */
-/*   Updated: 2022/04/08 17:22:17 by rgallego         ###   ########.fr       */
+/*   Updated: 2022/04/08 17:49:27 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <fcntl.h>
 # include <mlx.h>
 # include <math.h>
+# include "keys.h"
 # include "libft.h"
 # include "get_next_line.h"
 
@@ -36,6 +37,7 @@
 # define WHITE 0x00FFFFFF
 # define Z 0
 # define COLOUR	1
+# define ON_KEYDOWN 2
 
 /******************************** STRUCTURE ********************************* */
 typedef struct s_cell
@@ -82,7 +84,7 @@ typedef struct s_bresen
 	int		colour;
 	int		variation;
 	t_point	step;
-	t_point d;
+	t_point	d;
 }	t_bresen;
 
 /********************************** UTILS *********************************** */
---
+++ 42cursus/fdf/inc/keys.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keys.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgallego <rgallego@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/08 17:39:36 by rgallego          #+#    #+#             */
+/*   Updated: 2022/04/08 17:48:55 by rgallego         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEYS_H
+# define KEYS_H
+
+# define ON_KEYDOWN 2
+
+# define KEY_ESC 53
+# define KEY_LEFT 123
+# define KEY_RIGHT 124
+# define KEY_DOWN 125
+# define KEY_UP 126
+#endif
