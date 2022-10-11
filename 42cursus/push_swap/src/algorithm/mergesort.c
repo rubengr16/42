267e2817eef7bc0ca67a3fd8527f0448501f0d9e
RUBEN GALLEGO <ruben.gallegoramirezdeanton@telefonica.com>
2022-10-12 01:29:40 +0200

    small optimization

--- 42cursus/push_swap/src/algorithm/mergesort.c
+++ 42cursus/push_swap/src/algorithm/mergesort.c
@@ -6,12 +6,12 @@
 /*   By: rgallego <rgallego@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 17:09:01 by rgallego          #+#    #+#             */
-/*   Updated: 2022/10/11 23:49:39 by rgallego         ###   ########.fr       */
+/*   Updated: 2022/10/12 01:27:02 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "algorithm.h"
-
+#include <stdio.h>
 static void	make_divisions_btree_rec(t_bnode *a, t_bnode *b)
 {
 	t_bnode	*a_left;
@@ -64,6 +64,53 @@ static t_stack	*transform_btrees_to_stack(t_btree *a_btree, t_btree *b_btree)
 	return (num_stack);
 }
 
+ static int	is_oposite_r(char *first_mvnt, char *current_mvnt)
+ {
+	if (!ft_strncmp(first_mvnt, "ra", 2)
+		&& !ft_strncmp(current_mvnt, "rb", 2))
+		return (1);
+	if (!ft_strncmp(first_mvnt, "rb", 2)
+		&& !ft_strncmp(current_mvnt, "ra", 2))
+		return (1);
+	return (0);
+ }
+
+static void	optimizer(t_mvntslist *mvnts)
+{
+	t_lnode		*first_r;
+	int			cnt;
+	int			n_iter;
+
+	first_r = NULL;
+	cnt = 0;
+	n_iter = mvnts->n_elem;
+	while (n_iter)
+	{
+		if (!first_r && (!ft_strncmp(mvnts->head->mvnt, "ra", 2)
+			|| !ft_strncmp(mvnts->head->mvnt, "rb", 2)))
+			first_r = mvnts->head;
+		if (first_r && !ft_strncmp(mvnts->head->mvnt, first_r->mvnt, 2))
+			cnt++;
+		else if (cnt && is_oposite_r(first_r->mvnt, mvnts->head->mvnt))
+		{
+			first_r->mvnt[1] = 'r';
+			first_r = first_r->next;
+			if (cnt == 1)
+				first_r = NULL;
+			mvntslistdelone(mvnts);
+			mvnts->head = mvnts->head->prvs;
+			cnt--;
+		}
+		else
+		{
+			first_r = NULL;
+			cnt = 0;
+		}
+		mvnts->head = mvnts->head->next;
+		n_iter--;
+	}
+}
+
 void	ft_mergesort(t_push_swap push_swap, int half)
 {
 	t_btree	*a_btree;
@@ -77,6 +124,7 @@ void	ft_mergesort(t_push_swap push_swap, int half)
 	make_divisions_btree(a_btree, b_btree);
 	stack = transform_btrees_to_stack(a_btree, b_btree);
 	the_algorithm(push_swap, stack);
+	optimizer(push_swap.mvnts);
 	btreedelall(a_btree);
 	btreedelall(b_btree);
 	stackdelall(stack);
