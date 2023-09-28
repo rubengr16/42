ecfa33fc70f06a54693afc2cd7e067d583600ba0
rgallego <ruben.gallegoramirezdeanton@telefonica.com>
2023-09-28 22:11:11 +0200

    almost finished

--- 42cursus/cpp00/ex02/src/Account.cpp
+++ 42cursus/cpp00/ex02/src/Account.cpp
@@ -6,21 +6,37 @@
 /*   By: rgallego <rgallego@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 23:29:54 by rgallego          #+#    #+#             */
-/*   Updated: 2023/09/23 00:43:17 by rgallego         ###   ########.fr       */
+/*   Updated: 2023/09/28 21:30:46 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Account.hpp"
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
 Account::Account(int initial_deposit)
 {
-	
+	this->_accountIndex = _nbAccounts++;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	_displayTimestamp();
+	std::cout <<  "index:" << this->_accountIndex << ";"
+		<< "amount:" << this->_amount << ";"
+		<< "created";
 }
 
 Account::~Account(void)
 {
-	
+	_nbAccounts--;
+	_displayTimestamp();
+	std::cout <<  "index:" << this->_accountIndex << ";"
+		<< "amount:" << this->_amount << ";"
+		<< "closed";
 }
 
 
@@ -42,4 +58,36 @@ int	Account::getNbDeposits(void)
 int	Account::getNbWithdrawals(void)
 {
 	return (_totalNbWithdrawals);
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	this->_amount += deposit;
+	_totalNbDeposits++;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	this->_amount -= withdrawal;
+	_totalNbWithdrawals--;
+}
+
+int		Account::checkAmount(void) const
+{
+	return (this->_amount);
+}
+
+void	Account::displayStatus(void) const
+{
+	_nbAccounts--;
+	_displayTimestamp();
+	std::cout <<  "index:" << this->_accountIndex << ";"
+		<< "amount:" << this->_amount << ";"
+		<< "deposits:" << this->_nbDeposits << ";"
+		<< "withdrawals:" << this->_nbWithdrawals << ";";
+}
+
+void	_displayTimestamp( void )
+{
+	
 }
