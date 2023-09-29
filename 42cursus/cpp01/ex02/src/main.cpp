967274bc24e12395db100e03e8c40e427884f348
rgallego <rgallego@student.42madrid.com>
2023-09-29 17:29:52 +0200

    ex02 done

---
+++ 42cursus/cpp01/ex02/src/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgallego <rgallego@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 17:17:20 by rgallego          #+#    #+#             */
+/*   Updated: 2023/09/29 17:28:45 by rgallego         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	message = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &message;
+	std::string	&stringREF = message;
+
+	std::cout << "Address of message: " << &message
+		<< std::endl;
+	std::cout << "Address of stringPTR: " << stringPTR
+		<< std::endl;
+	std::cout << "Address of stringREF: " << &stringREF
+		<< std::endl;
+	std::cout << "Value of message: " << message
+		<< std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR
+		<< std::endl;
+	std::cout << "Value of stringREF: " << stringREF
+		<< std::endl;
+	return (0);
+}
--- 42cursus/cpp01/ex02/vbles.mk
+++ 42cursus/cpp01/ex02/vbles.mk
@@ -6,26 +6,19 @@
 #    By: rgallego <rgallego@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/09/04 22:49:11 by rgallego          #+#    #+#              #
-#    Updated: 2023/09/29 17:21:55 by rgallego         ###   ########.fr        #
+#    Updated: 2023/09/29 17:22:17 by rgallego         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRC_DIR	=			src/
 
-INC_DIR	=			inc/
-
-EX01	=			\
-					main.cpp \
-					Zombie.cpp \
-					zombieHorde.cpp
+EX02	=			main.cpp
 
 SRCS	= 			$(addprefix $(SRC_DIR), \
-						$(EX01) \
+						$(EX02) \
 					)
 
 OBJS	=			$(SRCS:.cpp=.o)
-
-DPNDS	=			$(SRCS:.cpp=.d)
 
 CC		=			c++
 
@@ -33,7 +26,4 @@ RM		=			-rm -f
 
 CFLAGS	=			-Wall -Wextra -Werror -std=c++98
 
-NAME	=			zombieHordes
-
-INC_ALL	=			\
-					$(INC_DIR)
+NAME	=			from_brain
