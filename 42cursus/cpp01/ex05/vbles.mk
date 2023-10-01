c72cc207a797d4d8580f5dd33aa327c6c81d7a33
rubengr16 <ruben16gallego@gmail.com>
2023-10-01 15:08:09 +0200

    begin ex05 structure

---
+++ 42cursus/cpp01/ex05/vbles.mk
@@ -0,0 +1,40 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    vbles.mk                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rgallego <rgallego@student.42madrid.com    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/09/04 22:49:11 by rgallego          #+#    #+#              #
+#    Updated: 2023/10/01 15:07:46 by rgallego         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SRC_DIR	=			src/
+
+INC_DIR	=			inc/
+
+EX05	=			\
+					main.cpp \
+					Harl.cpp
+
+SRCS	= 			$(addprefix $(SRC_DIR), \
+						$(EX05) \
+					)
+
+OBJS	=			$(SRCS:.cpp=.o)
+
+DPNDS	=			$(SRCS:.cpp=.d)
+
+CC		=			c++
+
+RM		=			-rm -f
+
+CFLAGS	=			-Wall -Wextra -Werror -std=c++98
+
+REPLACE_FILES	=	*.replace
+
+NAME	=			harl
+
+INC_ALL	=			\
+					$(INC_DIR)
