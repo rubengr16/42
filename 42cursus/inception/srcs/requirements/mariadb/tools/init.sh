b17e740603fed6e3b4e7df401550e5a7f7ca7919
rgallego <rgallego@student.42madrid.com>
2024-04-13 16:24:32 +0200

    it works, it is necessary to test how to configure to avoid certain opttions or more interpolation

--- 42cursus/inception/srcs/requirements/mariadb/tools/init.sh
+++ 42cursus/inception/srcs/requirements/mariadb/tools/init.sh
@@ -1,28 +1,27 @@
-if (! -d /data/mysql); then
+if [ ! -d /data/mysql ]; then
+	mariadb-install-db --no-defaults --skip-test-db --user=mysql
 
-	mariadb-install-db --no-defaults --skip-test-db
-
-	if ($? -ne 0)
+	if [ $? -ne 0 ]; then
 		echo "mariadb-install-db command failed!"
 		exit 1
 	fi
 
-	DB_ROOT_PASSWORD=< /dev/urandom tr -dc a-zA-Z0-9 | head -c20
-	echo "Please, copy and save the new root password: $(DB_ROOT_PASSWORD)"
+	DB_ROOT_PASSWORD=$(< /dev/urandom tr -dc a-zA-Z0-9 | head -c20)
+	echo "Please, copy and save the new root password: ${DB_ROOT_PASSWORD}"
 
-	cat << EOF | mariadbd --datadir=$(DB_DATA_DIR) --bootstrap --skip-grant-tables=false
+	cat << EOF | mariadbd --datadir=${DB_DATA_DIR} --bootstrap --skip-grant-tables=false --user=mysql
 
-		CREATE DATABASE IF NOT EXISTS $(DB_NAME);
-		CREATE USER IF NOT EXISTS $(DB_USER)
-			IDENTIFIED BY '$(DB_PASSWORD)';
-		GRANT ALL PRIVILEGES ON $(DB_NAME).* to $(DB_USER);
-		DROP USER 'mysql'@'localhost';
-		ALTER USER 'root'@'localhost' IDENTIFIED BY '$(DB_ROOT_PASSWORD)';
-		FLUSH PRIVILEGES;
+	CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+	CREATE USER IF NOT EXISTS ${DB_USER}
+		IDENTIFIED BY '${DB_PASSWORD}';
+	GRANT ALL PRIVILEGES ON ${DB_NAME}.* to ${DB_USER};
+	DROP USER 'mysql'@'localhost';
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+	FLUSH PRIVILEGES;
 
-	EOF
+EOF
 
-	if ($? -ne 0)
+	if [ $? -ne 0 ]; then
 		echo "mariadbd database initialization and users setup failed!"
 		exit 1
 	fi
