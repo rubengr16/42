fb5cff9d91bad4fd6a7c43c6eda59ae9350f8d0d
rubengr16 <ruben16gallego@gmail.com>
2026-05-02 18:38:14 +0200

    start python0

---
+++ 42cursus/python0/ex00/Hello.py
@@ -0,0 +1,15 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+ft_list[1] = "World!"
+ft_tuple = (ft_tuple[0], "Madrid!")
+ft_set.remove("tutu!")
+ft_set.add("Spain!")
+ft_dict["Hello"] = "42Madrid"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
