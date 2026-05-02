496864da94b5a609429e630410c0fbc9980f9690
rubengr16 <ruben16gallego@gmail.com>
2026-05-02 23:59:10 +0200

    ex02

---
+++ 42cursus/python0/ex02/find_ft_type.py
@@ -0,0 +1,9 @@
+def all_thing_is_obj(object: any) -> int:
+	object_cls = object.__class__
+	if object_cls == int:
+		print("Type not found")
+	elif object_cls == str:
+		print(f"{object} is in the kitchen : {object_cls}")
+	else:
+		print(f"{object_cls.__name__.capitalize()} : {object_cls}")
+	return 42
